24ba3bf86e0fc9cf519842dd50aa4ebd5e13d9f6
Dawit Bireda <davebireda2394@gmail.com>
2024-05-31 14:44:49 +0300

    course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -10,21 +10,34 @@ exports.getAllCourses = async (req, res) => {
     }
 };
 
-exports.createCourse = async (req, res) => {
-    const course = new Course({
-        course_name: req.body.course_name,
-        course_description: req.body.course_description,
-        start_date: req.body.start_date,
-        end_date: req.body.end_date,
-    });
 
+
+exports.createCourse = async (req, res) => {
     try {
+        // Check if a course with the same name already exists
+        const existingCourse = await Course.findOne({ course_name: req.body.course_name });
+        if (existingCourse) {
+            return res.status(400).json({ message: 'Course with this name already registered' });
+        }
+
+        // Create a new course if no existing course is found
+        const course = new Course({
+            course_name: req.body.course_name,
+            course_description: req.body.course_description,
+            start_date: req.body.start_date,
+            end_date: req.body.end_date,
+        });
+
         const newCourse = await course.save();
-        res.status(201).json(newCourse);
+        res.status(201).json({ 
+            message: 'Course successfully registered',
+            course: newCourse 
+        });
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
 };
+
 
 exports.getCourseById = async (req, res) => {
     try {
--- models/Student.js
+++ models/Student.js
@@ -5,7 +5,11 @@ const StudentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     date_of_birth: { type: Date, required: true },
-    enrollment_date: { type: Date, default: Date.now },
+    enrollment_date:
+    { type: Date, default:
+     Date.now 
+    },
 });
+
 
 module.exports = mongoose.model('Student', StudentSchema);
